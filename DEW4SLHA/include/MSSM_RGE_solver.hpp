b87391a9fcb76aa7b83bd1ac4bcf0dcf6e7f562a
Dmartinez-96 <85904612+Dmartinez-96@users.noreply.github.com>
2024-04-30 10:30:03 -0500

    Update MSSM_RGE_solver.hpp

--- DEW4SLHA/include/MSSM_RGE_solver.hpp
+++ DEW4SLHA/include/MSSM_RGE_solver.hpp
@@ -6,5 +6,7 @@
 #include <vector>
 
 void MSSMRGESolver(const std::vector<double>& x, std::vector<double>& dxdt, const double t);
+
 std::vector<double> solveODEs(std::vector<double> initialConditions, double startTime, double endTime, double timeStep);
+
 #endif
